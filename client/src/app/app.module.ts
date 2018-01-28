ddf3eada411e90903bc77e4dbcc1b8e501ee99aa
enjoyass@outlook.com <enjoyass@outlook.com>
2018-01-28 15:02:38 +0800

    add navbar componets

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavheaderComponent } from './componets/navheader/navheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavheaderComponent
   ],
   imports: [
     BrowserModule,
