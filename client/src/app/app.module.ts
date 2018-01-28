1c67c1e6e05f9e07d7bcdc84979c3bc53bd65f4a
enjoyass@outlook.com <enjoyass@outlook.com>
2018-01-28 20:13:54 +0800

    add routing module and modify navbarhtml file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app.routing.module';
 
 import { AppComponent } from './app.component';
 import { NavheaderComponent } from './componets/navheader/navheader.component';
+import { HomeComponent } from './componets/home/home.component';
+import { DashboardComponent } from './componets/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavheaderComponent
+    NavheaderComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
